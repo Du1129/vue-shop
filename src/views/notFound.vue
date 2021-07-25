da2082453936c6b5f75dc3cb448ffc6e22664f8e
Du1129 <15602241613@163.com>
2021-07-25 22:11:48 +0800

    new File

---
+++ src/views/notFound.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="center mainContent">
+        <img src="../assets/img/very_sorry.png"/>
+    </div>
+</template>
+
+<script>
+
+export default {
+//import引入的组件需要注入到对象中才能使用
+    name:'notFound',
+    components: {},
+    data() {
+        return {
+
+        }
+    },
+//监听属性 类似于data概念
+    computed: {},
+    created() {
+
+    },
+//生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() {
+
+    },
+
+
+
+//方法集合
+    methods: {
+
+    }
+}
+</script>
+<style lang='scss' scoped>
+.center{
+    display:flex;
+    justify-content: center;
+    min-width:800px;
+}
+
+</style>
